85ee96a60130b8de68f8aaeeab2dd88f0e912750
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    davinci: Build LiveDisplay HAL
    Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

--- device.mk
+++ device.mk
@@ -90,6 +90,10 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
