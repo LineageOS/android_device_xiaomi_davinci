287af64171a6aabf24cec182514625f9c9c7f5b8
Dan Willemsen <dwillemsen@google.com>
2018-04-04 16:49:07 -0700

    davinci: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
    Bug: 77599627
    Test: lunch  [see entries in list]
    Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_davinci.mk
+    $(LOCAL_DIR)/lineage_davinci.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_davinci-userdebug \
+    lineage_davinci-eng
