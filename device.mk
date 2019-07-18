29e2a8e99bea973f48727d59ad1ff09ff9e425b5
LuK1337 <priv.luk@gmail.com>
2019-07-18 19:41:39 +0200

    davinci: Set proper fingerprint sensor location
    Change-Id: I3cfc4d198c3a88724e3a268a4e9e9b7ab9909f9b

--- device.mk
+++ device.mk
@@ -9,7 +9,8 @@ $(call inherit-product-if-exists, vendor/xiaomi/davinci/davinci-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Permissions
 PRODUCT_COPY_FILES += \
