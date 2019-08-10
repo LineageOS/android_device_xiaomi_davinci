8eeef5d102c20ecbd26839d76154bf4462d1203d
PIPIPIG233666 <pig.priv@gmail.com>
2019-08-10 18:56:37 -0400

    davininci: Switch to vendor variants of custom HALs
    Change-Id: Id81c5e17a38d56bf23265068b6ec7f5bb3eb33d1

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_davinci
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_davinci
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
