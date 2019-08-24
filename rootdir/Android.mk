10200c6abdf41fc602f2cccd9dca0516231a9b4d
PIPIPIG233666 <pig.priv@gmail.com>
2019-08-24 17:51:31 -0400

    davinci: Build device specific packages and init script
    Change-Id: I4e50da5f92ed485d33f492b357e4dab993703491

---
+++ rootdir/Android.mk
@@ -0,0 +1,7 @@
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.davinci.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.davinci.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
+include $(BUILD_PREBUILT)
