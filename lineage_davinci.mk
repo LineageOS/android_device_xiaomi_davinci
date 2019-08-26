5dfe3d226516e4988d8cc6aed30dcfe2466794d1
AbhiShek Aggarwal <warabhishek@gmail.com>
2019-08-26 17:40:14 -0400

    davinci: Add custom init to set proper variants
    Change-Id: I04f09a14cf41c89b3135c53e3089d5c3e703aae0

--- lineage_davinci.mk
+++ lineage_davinci.mk
@@ -17,3 +17,5 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/davinci/davinci:10/QKQ1.190825.002/V11.0.2.0.QFJCNXM:user/release-keys"
