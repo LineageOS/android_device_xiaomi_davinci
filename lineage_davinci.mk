299a77fc886df5a1880ad9ca2ee38485beb67a5f
PIPIPIG233666 <pig.priv@gmail.com>
2019-09-04 19:54:16 -0400

    davinci: Pin display calibration files
     * from davinci-user 9 PKQ1.190302.001 V10.3.15.0.PFJCNXM release-keys
     * Blacklist ro.product.model as well.
    
    Change-Id: Icc717e66ecfd9c6f8b05a1bdb8e6c341b8951600

--- lineage_davinci.mk
+++ lineage_davinci.mk
@@ -14,14 +14,16 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_NAME := lineage_davinci
 PRODUCT_DEVICE := davinci
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi K20
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/davinci/davinci:9/PKQ1.190302.001/V10.3.11.0.PFJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/davinci/davinci:9/PKQ1.190302.001/V10.3.15.0.PFJCNXM:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="davinci-user 9 PKQ1.190302.001 V10.3.11.0.PFJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="davinci-user 9 PKQ1.190302.001 V10.3.15.0.PFJCNXM release-keys" \
     PRODUCT_NAME="davinci" \
     TARGET_DEVICE="davinci"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
+    ro.product.model
