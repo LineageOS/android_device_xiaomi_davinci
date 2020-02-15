ce3d4efff6301b1358f5ce48cecf72be6e378a05
Bruno Martins <bgcngm@gmail.com>
2020-02-15 19:16:30 +0000

    davinci: Set shipping API level
    Moved from common tree as not all targets were launched with Android 9.
    
    Change-Id: Ie8e9f46cdf7f90b1463204d97379d76914908d97

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/davinci/davinci-vendor.mk)
 
