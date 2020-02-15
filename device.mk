0d3383493693cdaed7f3e3f3878762245bfcaf6e
LuK1337 <priv.luk@gmail.com>
2020-02-15 21:49:40 +0100

    davinci: Inherit product_launched_with_p.mk
    Change-Id: I0738ccbb8de3f6aac1dd7d937eaccc35e0fc86ae

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# API level, the device has been commercially launched on
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+
 # Inherit from sm6150-common
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
