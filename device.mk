fec044eef55dff67e4128452c3f6eece32c75c31
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-17 21:32:28 -0500

    davinci: Remove FlickerFree
    Revert "davinci: FlickerFree: Migrate to androidx"
    
    This reverts commit edf12280ffdb814796d0f706a0ae42f41a564d7d.
    
    Change-Id: I760d17bd93ae14dce1653e840553f257557f3ea3
    
    Revert "davinci: Implement flicker-free for DC dimming"
    
    This reverts commit 6e802fb8524f4afad5bee6f39dbcc70df13ddc61.
    
    Change-Id: I27360320be66b11bcceb9736ee625571a286bcd5

--- device.mk
+++ device.mk
@@ -60,10 +60,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_davinci.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_davinci.rc \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
-# FlickerFree
-PRODUCT_PACKAGES += \
-    FlickerFree
-
 # HIDL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/manifest_sdmmagpie.xml:$(TARGET_COPY_OUT_VENDOR)/odm/etc/vintf/manifest_365.xml \
