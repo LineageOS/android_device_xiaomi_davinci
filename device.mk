bad3033291b5647164ffc504465fc8d9eace02bf
Pig <pig.priv@gmail.com>
2020-03-23 22:27:45 +0100

    davinci: Build fingerprint wrapper from source
    Change-Id: I920a3b988a942c3fe30289ca24afd4cc881b13e4

--- device.mk
+++ device.mk
@@ -56,7 +56,6 @@ PRODUCT_PACKAGES += \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fingerprint/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_davinci.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.xiaomi_davinci.rc \
     vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 
 # HIDL
