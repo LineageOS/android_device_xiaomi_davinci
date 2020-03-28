233851bb0dbfeab1c58d609dbc456f01a3ca6f28
Demon000 <demonsingur@gmail.com>
2020-03-28 22:03:25 +0100

    Revert "davinci: Switch to AOSP NFC"
    This reverts commit 238f80a23a7a98400519fa875f4ba8e692e29dee.
    
    Change-Id: I1f82959d9d5f8654b6fb443cadcba2e73c48d463

--- device.mk
+++ device.mk
@@ -67,12 +67,20 @@ PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.xiaomi_sm6150
 
 # NFC
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/nxp/opensource/sn100x
+
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
+    com.gsma.services.nfc \
+    com.nxp.nfc.nq \
+    libnqnfc_nci_jni \
+    nfc_nci.nqx.default.hw \
+    NQNfcNci \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
     Tag \
-    android.hardware.nfc@1.2-service
+    vendor.nxp.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
