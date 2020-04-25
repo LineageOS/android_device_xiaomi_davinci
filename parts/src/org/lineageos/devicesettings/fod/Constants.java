4b0ca63d7f2d11d9236a6e880da44bc2ab3da586
Pig <pig.priv@gmail.com>
2020-04-25 17:25:17 +0200

    davinci: parts: Add constants
    Change-Id: I6de7e5dbbcb81d7a47bd3c7eaaa0d15e8d947420

---
+++ parts/src/org/lineageos/devicesettings/fod/Constants.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.devicesettings.fod;
+
+public class Constants {
+  public static final int SCREEN_ADAPT = 0;
+  public static final int SCREEN_ENHANCE = 1;
+  public static final int SCREEN_EXPERT = 26;
+  public static final int SCREEN_EYECARE = 3;
+  public static final int SCREEN_GAME_HDR = 19;
+  public static final int SCREEN_HIGHLIGHT = 11;
+  public static final int SCREEN_MONOCHROME = 4;
+  public static final int SCREEN_NIGHTLIGHT = 9;
+  public static final int SCREEN_STANDARD = 2;
+  public static final int SCREEN_SUNLIGHT = 8;
+}
