2b6572b625d879f2431651bdd7a4e64395bf6ac5
TheScarastic <warabhishek@gmail.com>
2020-05-25 10:55:56 +0000

    davinci: Build fod surfaceflinger extension lib
    Change-Id: I51fb56c0a20e3cac83afb46ed6a6e2f967159d18

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Assert
 TARGET_OTA_ASSERT_DEVICE := davinci,davinciin
 
+# Fingerprint
+TARGET_SURFACEFLINGER_FOD_LIB := //$(COMMON_PATH):libfod_extension
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_davinci
 TARGET_RECOVERY_DEVICE_MODULES := libinit_davinci
