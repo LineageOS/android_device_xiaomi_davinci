57b40bab7e042e6c793868888141083880c5b3a0
Anaple <67675386+Anaple@users.noreply.github.com>
2020-08-26 18:35:10 +0800

    Update setup-makefiles.sh

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -13,29 +13,7 @@ export DEVICE_COMMON=sm6150-common
 export VENDOR=xiaomi
 
 export DEVICE_BRINGUP_YEAR=2019
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-LINEAGE_ROOT="${MY_DIR}/../../.."
-
-HELPER="${LINEAGE_ROOT}/vendor/exthm/build/tools/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}"
-
-# Copyright headers and guards
-write_headers
-
-# The standard blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 
 "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
 
-# Finish
-write_footers
