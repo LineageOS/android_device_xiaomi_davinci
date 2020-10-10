1c1aed530c3899374651c26ee145b1bf6c2c73f8
PIPIPIG233666 <pig.priv@gmail.com>
2020-10-10 12:32:28 -0400

    davinci: Import Xiaomi changes over audio policy engine configs
     * From miui_DAVINCI_20.9.17_e4423c6436_10.0.
     * Reroute A2DP to HEADSET as per AOSP behavior.
    
    Change-Id: I22ebcaf00e7e8321dcb612f101eb5f19a3b58dcd

--- device.mk
+++ device.mk
@@ -36,10 +36,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
-    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/audio/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Camera
 PRODUCT_PACKAGES += \
