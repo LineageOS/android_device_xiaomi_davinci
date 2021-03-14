a24aa2a15bd6c633867f49a0fb87f6aec232d8a7
Arian <arian.kulmer@web.de>
2021-03-14 13:21:16 +0100

    davinci: Inherit from sm6150-common
    Change-Id: Id73d4848a3e2dc12fceef306b616558efe9335c2

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sm6150-common
+$(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
