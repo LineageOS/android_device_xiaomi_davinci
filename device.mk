d2f98560d7e849629564db65b7e1e332345f3372
Arian <arian.kulmer@web.de>
2021-05-29 22:26:01 +0200

    davinci: Build audio HAL and audio modules
    Change-Id: I9aae0d727ff8ee216c24760b18fab1cc139a2d28

--- device.mk
+++ device.mk
@@ -11,6 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 
 # Audio
+PRODUCT_PACKAGES += \
+    audio.primary.sm6150
+
+PRODUCT_PACKAGES += \
+    liba2dpoffload \
+    libhdmiedid \
+    libhfp \
+    libsndmonitor \
+    libspkrprot
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml \
