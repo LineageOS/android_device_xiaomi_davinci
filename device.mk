35cace610dd6d2ec8ca012ef1914dcbcb57bd35d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-23 16:49:09 +0200

    davinci: Move to common Xiaomi light AIDL
    Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352

--- device.mk
+++ device.mk
@@ -55,7 +55,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm6150
+    android.hardware.light-service.xiaomi
 
 # NFC
 PRODUCT_PACKAGES += \
