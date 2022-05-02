62e29320bd8f4984451b6cbf630c36eab733c536
Bruno Martins <bgcngm@gmail.com>
2022-05-02 11:28:36 +0100

    davinci: Move to common fingerprint HIDL
    Change-Id: I7cb0a45c865d9dbfd800b1d633f8d5ae613bb1b9

--- device.mk
+++ device.mk
@@ -42,7 +42,6 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sm6150-udfps \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # Fstab
