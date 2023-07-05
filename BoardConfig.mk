675d73a0591f3cf727484ab32c0647c9e6997328
Arian <arian.kulmer@web.de>
2023-07-05 09:23:34 +0200

    davinci: Drop NFC SKU
    Change-Id: I7d5ab7f8855367fb09ca4c0e8732886164425ca5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,9 +21,6 @@ BOARD_USES_ALSA_AUDIO := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
-ODM_MANIFEST_SKUS += davinci
-ODM_MANIFEST_DAVINCI_FILES := \
-    $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_davinci
