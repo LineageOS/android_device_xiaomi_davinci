a30629c138fd8dccd3d2fb86cac68d98485fac30
basamaryan <basam.aryan@gmail.com>
2024-11-20 03:45:30 -0500

    davinci: Enable elfchecks for libgrallocutils
    Change-Id: I3719a100caa3535d3f2c27f4d2a0b9f75674d1d7

--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -19,6 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/sm8150',
     'hardware/xiaomi',
     'vendor/qcom/opensource/display',
     'vendor/xiaomi/sm6150-common',
@@ -26,9 +26,7 @@ namespace_imports = [
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    'libgrallocutils': lib_fixup_remove,
 }
-
 
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
