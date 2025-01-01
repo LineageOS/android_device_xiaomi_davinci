00b49161eb4b138a2cb5c7b973ca504ec38ed0ff
Michael Bestas <mkbestas@lineageos.org>
2025-01-02 01:12:07 +0200

    davinci: Disable OMX service
    Change-Id: I5381e098af64271f9d71c484262e1710e8fcacea

--- lineage_davinci.mk
+++ lineage_davinci.mk
@@ -1,11 +1,12 @@
 #
-# Copyright (C) 2021-2024 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from davinci device
